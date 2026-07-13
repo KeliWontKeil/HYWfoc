433f12772ec72fd23c20ca604ef051f2bbd3ec87
KeliWontKeil <2603004604@qq.com>
2026-07-13 14:05:12 +0800

    移除无用文件的跟踪

--- examples/GD32F303_FOCExplore/software/output/math_transforms.d
+++
@@ -1,8 +0,0 @@
-.\output\math_transforms.o: ..\..\..\foc\src\L41_Math\math_transforms.c
-.\output\math_transforms.o: ..\..\..\foc\include\L41_Math/math_transforms.h
-.\output\math_transforms.o: D:\Keil_v5_ARM\ARM\ARMCC\Bin\..\include\math.h
-.\output\math_transforms.o: D:\Keil_v5_ARM\ARM\ARMCC\Bin\..\include\stdint.h
-.\output\math_transforms.o: ..\..\..\foc\include\L41_Math/foc_math_lut.h
-.\output\math_transforms.o: ..\..\..\foc\include\LS_Config/foc_cfg_init_values.h
-.\output\math_transforms.o: ..\..\..\foc\include\LS_Config/foc_symbol_defs.h
-.\output\math_transforms.o: ..\..\..\foc\include\LS_Config/foc_cfg_feature_switches.h
